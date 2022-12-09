99e254805231c120da4e0f7669079204c77b923d
andrew <changtime247@gmail.com>
2022-12-08 19:47:06 -0500

    mongoose strictQuery true added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
+mongoose.set('strictQuery', true)
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
